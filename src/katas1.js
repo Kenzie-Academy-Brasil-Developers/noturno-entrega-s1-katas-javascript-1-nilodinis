728c421f52489fc4b7ea3f834f733e584216d692
Nilo Dinis <nilodinis@hotmail.com>
2021-11-05 00:08:15 -0400

    Corrigido_identação

--- src/katas1.js
+++ src/katas1.js
@@ -4,8 +4,9 @@ function oneThroughTwenty() {
   let n=1
   let numero=[]
   while(n<=20) {//loop para números de 1 a 20
-  numero.push(n)
-  n++}
+    numero.push(n)
+    n++
+  }
   return numero
 }
 console.log(oneThroughTwenty())//Imprimir os números
@@ -16,12 +17,14 @@ function evensToTwenty() {
   let n=1
   let numero=[]
   while(n<=20) {//loop para números de 1 a 20
-  if (n%2==0){//Condição para ser número par
-  numero.push(n)}
-  n++}  
+    if (n%2==0){//Condição para ser número par
+    numero.push(n)
+    }
+  n++
+  }  
   return numero  
 }
-  console.log(evensToTwenty())//Imprimir os números
+console.log(evensToTwenty())//Imprimir os números
  
 //---------------------------------------------------------
   //Função para listar os números ímpares de 1 a 20
@@ -29,12 +32,14 @@ function oddsToTwenty() {
   let n=1
   let numero=[]
   while(n<=20){//loop para números de 1 a 20
-  if (n%2!=0){//Condição para ser número ímpar
-  numero.push(n)}
-  n++}
-  return numero
+    if (n%2!=0){//Condição para ser número ímpar
+    numero.push(n)
+    }
+  n++
   }
-  console.log(oddsToTwenty())//Imprimir os números
+  return numero
+}
+console.log(oddsToTwenty())//Imprimir os números
  
   //--------------------------------------------------------
 //Função para retornar os múltiplos de 5 até 100
@@ -42,9 +47,11 @@ function multiplesOfFive() {
   let n=5
   let numero=[]
   while(n<=100){//loop para números de 5 até 100
-  if (n%5==0){//Condição para ser múltiplo de 5
-  numero.push(n)}
-  n++}
+    if (n%5==0){//Condição para ser múltiplo de 5
+    numero.push(n)
+    }
+  n++
+  } 
   return numero
 }
 console.log(multiplesOfFive())//Imprimir os números
@@ -56,9 +63,11 @@ function squareNumbers() {
   let n=1
   let numero=[]
   while(n<=100){//loop para os números de 1 a 100
-  if (Math.sqrt(n)%1===0){//Condição para ser quadrado perfeito
-  numero.push(n)} 
-  n++}  
+    if (Math.sqrt(n)%1===0){//Condição para ser quadrado perfeito
+    numero.push(n)
+    } 
+  n++
+  }  
   return numero
 }
 
@@ -68,12 +77,13 @@ console.log(squareNumbers())//Imprimir os números
 //Função para retornar os números de 20 a 1
 function countingBackwards() {
     
-let n=20
-let numero=[]
-while(n>=1){//loop para os números de 20 a 1
-numero.push(n)
-n=n-1}
-return numero
+  let n=20
+  let numero=[]
+  while(n>=1){//loop para os números de 20 a 1
+    numero.push(n)
+    n=n-1
+  }
+  return numero
 }
 console.log(countingBackwards())//Imprimir os números
 
@@ -81,13 +91,15 @@ console.log(countingBackwards())//Imprimir os números
 //Função para retornar os números pares de 20 a 1
 function evenNumbersBackwards() {
 
-let numero=[]
-let n=20
-while (n>=1){//Loop para os números de 20 a 1
-if(n%2==0){//Condição para números pares
-numero.push(n)}
-n=n-1}
-return numero
+  let numero=[]
+  let n=20
+  while (n>=1){//Loop para os números de 20 a 1
+    if(n%2==0){//Condição para números pares
+    numero.push(n)
+    }
+  n=n-1
+  }
+  return numero
 }
 console.log(evenNumbersBackwards())//Imprimir os números
 
@@ -98,9 +110,11 @@ function oddNumbersBackwards() {
   let numero=[]
   let n=20
   while (n>=1){//loop para os números de 20 a 1
-  if(n%2!=0){//Condição para números ímpares
-  numero.push(n)}
-  n=n-1  }
+    if(n%2!=0){//Condição para números ímpares
+    numero.push(n)
+    }
+  n=n-1  
+  }
   return numero
 }
 console.log(oddNumbersBackwards())//Imprimir os números
@@ -114,9 +128,12 @@ function multiplesOfFiveBackwards() {
   let numero=[]
   let n=100
   while (n>=1){//lopp para números de 100 a 1
-  if(n%5==0){//Condição para múltiplo de 5
-  numero.push(n)}
-  n=n-1  }
+    if(n%5==0){//Condição para múltiplo de 5
+    numero.push(n)
+    }
+  n=n-1  
+  }
+  return numero
 }
 console.log(multiplesOfFiveBackwards())
 
@@ -128,9 +145,12 @@ function squareNumbersBackwards() {
   let numero=[]
   let n=100
   while (n>=1){//Loop para os números de 100 a 1
-  if (Math.sqrt(n)%1===0){
-  numero.push(n)} 
-  n=n-1  }
+    if (Math.sqrt(n)%1===0){
+    numero.push(n)
+    } 
+  n=n-1  
+  }
+  return numero
 }
 console.log(squareNumbersBackwards())
 
